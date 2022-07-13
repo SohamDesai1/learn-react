ed20668ab36713c96d5d054cad758f89f4c88bd5
SohamDesai1 <sohamdesai123@gmail.com>
2022-07-13 19:49:30 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import './demo.css';
-import  Props  from "./components/Props";
- let a = 'Soham';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <><div className="demo">
-      <h1>Hello World</h1>
-      <p>This is a simple React app</p>
-      <p>{a}</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    <Props num = 'second'/>
-    </>
-    
   );
 }
 
